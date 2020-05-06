2ae71deb277d2ae10ab69eae5cd3052a29b7f19c
Matheus Fideles <matheuss.fideles@hotmail.com>
2020-05-06 18:51:06 -0300

    Extraindo o email do token e armazendos no localstorage

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,9 +4,14 @@ import { API_CONFIG } from "./../config/api.config";
 import { CredenciasDTO } from "./../models/credenciais.dto";
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { JwtHelper } from 'angular2-jwt';
+
 
 @Injectable()
 export class AuthService {
+
+  jwtHelper: JwtHelper = new JwtHelper();
+
   constructor(public http: HttpClient, public storage: StorageService) {}
 
   authenticate(creds: CredenciasDTO) {
@@ -24,7 +29,8 @@ export class AuthService {
   sucessfullLogin(authorizationValue : string) {
     let tok = authorizationValue.substring(7);
     let user: LocalUser = {
-      token: tok
+      token: tok,
+      email: this.jwtHelper.decodeToken(tok).sub
     };
     this.storage.setLocalUser(user);
   }
@@ -32,5 +38,5 @@ export class AuthService {
   logout() {
     this.storage.setLocalUser(null);
   }
-  
+
 }
