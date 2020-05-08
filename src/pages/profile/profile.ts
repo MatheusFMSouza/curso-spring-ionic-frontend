e5084ec939d296518d8d3f177a07c8c413fa9b44
Matheus Fideles <matheuss.fideles@hotmail.com>
2020-05-08 12:07:29 -0300

    monstrando dados e imagem de cliente para a pagina de profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,9 @@
+import { ClienteService } from "./../../services/domain/cliente.service";
 import { StorageService } from "./../../services/storage.service";
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { ClienteDTO } from "../../models/Cliente.dto";
+import { API_CONFIG } from "../../config/api.config";
 
 @IonicPage()
 @Component({
@@ -8,18 +11,34 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
   templateUrl: "profile.html",
 })
 export class ProfilePage {
-  email: string;
+  cliente: ClienteDTO;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public storage: StorageService
+    public storage: StorageService,
+    public service: ClienteService
   ) {}
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
-      this.email = localUser.email;
+      this.service.findByEmail(localUser.email).subscribe(
+        (response) => {
+          this.cliente = response;
+          //buscar imagem do usuario no bucket
+          this.getImageIfExists();
+        },
+        (error) => {}
+      );
     }
+  }
+  getImageIfExists() {
+    this.service.getImageFromBucket(this.cliente.id).subscribe(
+      (response) => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      },
+      (error) => {}
+    );
   }
 }
