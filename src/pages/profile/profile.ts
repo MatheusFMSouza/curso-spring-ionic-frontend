8615b3940c2454f255860c8be566d1336ececbd2
Matheus Fideles <matheuss.fideles@hotmail.com>
2020-05-21 19:55:49 -0300

    usando cordova para tirar foto com a camera

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,50 +1,132 @@
-import { ClienteService } from "./../../services/domain/cliente.service";
-import { StorageService } from "./../../services/storage.service";
-import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
-import { ClienteDTO } from "../../models/Cliente.dto";
-import { API_CONFIG } from "../../config/api.config";
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { StorageService } from '../../services/storage.service';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { API_CONFIG } from '../../config/api.config';
+import { CameraOptions, Camera, CameraOriginal } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
-  selector: "page-profile",
-  templateUrl: "profile.html",
+  selector: 'page-profile',
+  templateUrl: 'profile.html',
 })
 export class ProfilePage {
+
   cliente: ClienteDTO;
+  picture: string;
+  profileImage;
+  cameraOn: boolean = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public service: ClienteService
-  ) {}
+    public clienteService: ClienteService,
+    public camera: CameraOriginal,
+    public sanitizer: DomSanitizer) {
+
+      this.profileImage = 'assets/imgs/avatar-blank.png';
+  }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
-      this.service.findByEmail(localUser.email).subscribe(
-        (response) => {
+      this.clienteService.findByEmail(localUser.email)
+        .subscribe(response => {
           this.cliente = response as ClienteDTO;
-          //buscar imagem do usuario no bucket
           this.getImageIfExists();
         },
-        (error) => {
+        error => {
           if (error.status == 403) {
             this.navCtrl.setRoot('HomePage');
           }
-        }
-      );
-    } else {
+        });
+    }
+    else {
       this.navCtrl.setRoot('HomePage');
     }
   }
+
   getImageIfExists() {
-    this.service.getImageFromBucket(this.cliente.id).subscribe(
-      (response) => {
-        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
-      },
-      (error) => {}
-    );
+    this.clienteService.getImageFromBucket(this.cliente.id)
+    .subscribe(response => {
+      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      this.blobToDataURL(response).then(dataUrl => {
+        let str : string = dataUrl as string;
+        this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      });
+    },
+    error => {
+      this.profileImage = 'assets/imgs/avatar-blank.png';
+    });
+  }
+
+  // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+  blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+        let reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = (e) => fulfill(reader.result);
+        reader.readAsDataURL(blob);
+    })
+  }
+
+  getCameraPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+      this.cameraOn = false;
+    });
+  }
+
+  getGalleryPicture() {
+
+    this.cameraOn = true;
+
+    const options: CameraOptions = {
+      quality: 100,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     this.picture = 'data:image/png;base64,' + imageData;
+     this.cameraOn = false;
+    }, (err) => {
+      this.cameraOn = false;
+    });
+  }
+
+ // sendPicture() {
+   // this.clienteService.uploadPicture(this.picture)
+     // .subscribe(response => {
+       // this.picture = null;
+       // this.getImageIfExists();
+      //},
+     // error => {
+     // });
+ // }
+
+  cancel() {
+    this.picture = null;
   }
 }
